386da1829d9e06d20919751e0ed27fc30433ffc5
Jaecheol Park <nature9th@gmail.com>
2021-11-05 23:48:13 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,34 +1,40 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+morgan('combined', {
+  skip: function (req, res) { return res.statusCode < 400 }
+})
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === '/protected') {
-    return res.send('<h1>Not Allowed</h1>');
-  }
-  next();
-};
+// const methodLogger = (req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   next();
+// };
 
-const hanldeHome = (req, res, next) => {
+// const privateMiddleware = (req, res, next) => {
+//   const url = req.url;
+//   if (url === '/protected') {
+//     return res.send('<h1>Not Allowed</h1>');
+//   }
+//   next();
+// };
+
+const home = (req, res) => {
   return res.send("<h1>안녕하세요 홈페이지입니다.</h1><p>Node.js 학습중입니다</p>");
 };
 
-const handleLogin = (req, res) => {
+const login = (req, res) => {
   return res.send("<h1>Login here.</h1>");
 };
 
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", hanldeHome);
-app.get("/login", handleLogin);
+// app.use(methodLogger, privateMiddleware);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
