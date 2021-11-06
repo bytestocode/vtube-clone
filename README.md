e0c04beeb57b7ec39b6b03b61485217d315467ca
Jaecheol Park <nature9th@gmail.com>
2021-11-06 21:40:49 +0900

    router

---
+++ README.md
@@ -0,0 +1,18 @@
+# Vtube Reloaded
+
+# Global Router
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+# User Router
+/users/edit -> Edit User
+/users/delete -> Delete User
+
+# Video Router
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/vidoes/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,40 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
 
-morgan('combined', {
-  skip: function (req, res) { return res.statusCode < 400 }
-})
-
-// const methodLogger = (req, res, next) => {
-//   console.log(`${req.method} ${req.url}`);
-//   next();
-// };
-
-// const privateMiddleware = (req, res, next) => {
-//   const url = req.url;
-//   if (url === '/protected') {
-//     return res.send('<h1>Not Allowed</h1>');
-//   }
-//   next();
-// };
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const home = (req, res) => {
-  return res.send("<h1>안녕하세요 홈페이지입니다.</h1><p>Node.js 학습중입니다</p>");
+  return res.send(
+    "<h1>안녕하세요 홈페이지입니다.</h1><p>Node.js 학습중입니다</p>"
+  );
 };
-
-const login = (req, res) => {
-  return res.send("<h1>Login here.</h1>");
-};
-
-app.use(logger);
-// app.use(methodLogger, privateMiddleware);
-app.get("/", home);
-app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
