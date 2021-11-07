9ce639200fcfb82b3fdc6326031743e5991534b1
Jaecheol Park <nature9th@gmail.com>
2021-11-07 22:00:55 +0900

    feat: URL Parameter

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import morgan from 'morgan';
-import globalRouter from './routers/globalRouter';
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
@@ -13,12 +13,6 @@ app.use(logger);
 app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
-
-const home = (req, res) => {
-  return res.send(
-    '<h1>안녕하세요 홈페이지입니다.</h1><p>Node.js 학습중입니다</p>'
-  );
-};
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
