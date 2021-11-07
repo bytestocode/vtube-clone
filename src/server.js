415fa6e41c162ebeb32ee8ba549ce0127dce911a
Jaecheol Park <nature9th@gmail.com>
2021-11-07 16:46:04 +0900

    export routers controllers

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
-import express from "express";
-import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
-import videoRouter from "./routers/videoRouter";
+import express from 'express';
+import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const PORT = 4000;
 
 const app = express();
-const logger = morgan("dev");
+const logger = morgan('dev');
 app.use(logger);
 
-app.use("/", globalRouter);
-app.use("/users", userRouter);
-app.use("/videos", videoRouter);
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
 
 const home = (req, res) => {
   return res.send(
-    "<h1>안녕하세요 홈페이지입니다.</h1><p>Node.js 학습중입니다</p>"
+    '<h1>안녕하세요 홈페이지입니다.</h1><p>Node.js 학습중입니다</p>'
   );
 };
 
